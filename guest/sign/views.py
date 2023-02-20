d64450d33c0a348e85fd9f61a14796d1593ce240
cun <1129041490@qq.com>
2023-02-20 22:47:36 +0800

    使用cookie和session以及认证系统

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -12,12 +14,25 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
-        if username == 'admin' and password == 'admin123':
-            return HttpResponseRedirect('/event_manage/')
+        # if username == 'admin' and password == 'admin123':
+        #     response = HttpResponseRedirect('/event_manage/')
+        #     # response.set_cookie('user', username, 3600)
+        #     request.session['user'] = username
+        #     return response
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            request.session['user'] = username
+            response = HttpResponseRedirect('/event_manage/')
+            return response
+
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
 
 
 # 发布会管理
+@login_required
 def event_manage(request):
-    return render(request, 'event_manage.html')
+    # username = request.COOKIES.get('user', '')
+    username = request.session.get('user', '')
+    return render(request, 'event_manage.html', {"user": username})
