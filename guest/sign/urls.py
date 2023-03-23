4c17960555235785eec41e54497458a88a1e65f3
bcchen <1129041490@qq.com>
2023-03-23 09:36:22 +0800

    接口开发学习

--- guest/sign/urls.py
+++ guest/sign/urls.py
@@ -15,20 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from sign import views
+from sign import views_if
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/$', views.index),
-    url(r'^login_action/$', views.login_action),
-    url(r'^event_manage/$', views.event_manage),
-    url(r'^accounts/login/$', views.index),
-    url(r'^guest_manage/$', views.guest_manage),
-    url(r'^search_name/$', views.search_name),
-    url(r'^search_guest_name/$', views.search_guest_name),
-    url(r'^sign_index/(?P<eid>[0-9]+)/$', views.sign_index),
-    url(r'^sign_index_action/(?P<eid>[0-9]+)/$', views.sign_index_action),
-    url(r'^logout/$', views.logout),
-    url(r'^api/', include('sign.urls', namespace="sign")),
+    url(r'^add_event/', views_if.add_event, name='add_event'),
+    url(r'^add_guest/', views_if.add_guest, name='add_guest'),
+    url(r'^get_event_list/', views_if.get_event_list, name='get_event_list'),
+    url(r'^get_guest_list/', views_if.get_guest_list, name='get_guest_list'),
+    url(r'^user_sign/', views_if.user_sign, name='user_sign'),
 ]
